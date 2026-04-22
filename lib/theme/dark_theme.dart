ce3c5537e87438b43672d06996cbc97c8535f6fa
Mohit koley <mohitkoley.roldrive@gmail.com>
2026-04-22 22:54:25 +0530

    added

--- lib/theme/dark_theme.dart
+++ lib/theme/dark_theme.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sixam_mart/util/app_constants.dart';
 
-ThemeData dark({Color color = const Color(0xFF54b46b)}) => ThemeData(
+ThemeData dark({Color color = const Color(0xFF328096)}) => ThemeData(
   fontFamily: AppConstants.fontFamily,
   primaryColor: color,
   secondaryHeaderColor: const Color(0xFF009f67),
@@ -11,15 +11,28 @@ ThemeData dark({Color color = const Color(0xFF54b46b)}) => ThemeData(
   cardColor: const Color(0xFF30313C),
   shadowColor: Colors.white.withValues(alpha: 0.03),
   textTheme: const TextTheme(bodyMedium: TextStyle(color: Colors.white70)),
-  textButtonTheme: TextButtonThemeData(style: TextButton.styleFrom(foregroundColor: color)),
-  colorScheme: ColorScheme.dark(primary: color, secondary: color).copyWith(surface: const Color(0xFF191A26)).copyWith(error: const Color(0xFFdd3135)),
-  popupMenuTheme: const PopupMenuThemeData(color: Color(0xFF29292D), surfaceTintColor: Color(0xFF29292D)),
+  textButtonTheme: TextButtonThemeData(
+    style: TextButton.styleFrom(foregroundColor: color),
+  ),
+  colorScheme: ColorScheme.dark(primary: color, secondary: color)
+      .copyWith(surface: const Color(0xFF191A26))
+      .copyWith(error: const Color(0xFFdd3135)),
+  popupMenuTheme: const PopupMenuThemeData(
+    color: Color(0xFF29292D),
+    surfaceTintColor: Color(0xFF29292D),
+  ),
   dialogTheme: const DialogThemeData(surfaceTintColor: Colors.white10),
-  floatingActionButtonTheme: FloatingActionButtonThemeData(shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(500))),
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(500)),
+  ),
   bottomAppBarTheme: const BottomAppBarThemeData(
-    surfaceTintColor: Colors.black, height: 60,
+    surfaceTintColor: Colors.black,
+    height: 60,
     padding: EdgeInsets.symmetric(vertical: 5),
   ),
-  dividerTheme: const DividerThemeData(thickness: 0.5, color: Color(0xFFA0A4A8)),
+  dividerTheme: const DividerThemeData(
+    thickness: 0.5,
+    color: Color(0xFFA0A4A8),
+  ),
   tabBarTheme: const TabBarThemeData(dividerColor: Colors.transparent),
 );
